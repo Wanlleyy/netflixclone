e75d98132f09badbd51f3ab5799f06edcc95db3b
Wanlley silva <wanlley.silva@gmail.com>
2022-09-08 05:29:21 -0300

    adicionando ícones

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 import './FeaturedMovie.css';
+import { 
+    MdOutlineDoubleArrow
+ } from 'react-icons/md';
+
 
 export default ({item}) => {
 
@@ -28,7 +32,7 @@ export default ({item}) => {
                     </div>
                     <div className="featured--description">{item.overview}</div>
                     <div className="featured--buttons">
-                        <a href={`/watch/${item.id}`} className="featured--watchbutton"> Assistir</a>
+                        <a href={`/watch/${item.id}`} className="featured--watchbutton"><MdOutlineDoubleArrow className="featured--watchbuttonarrow"/> Assistir</a>
                         <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
                     </div>
                     <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
